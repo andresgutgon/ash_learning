ab3f7284d8269a3e460ea6d3fc720b28918c9013
andresgutgon <andresgutgon@gmail.com>
2025-12-07 16:17:59 +0100

    Nice refactor making an action more modular
    I dont like my resource crowded with action's code. I think I like this
    way more

--- lib/ash_learning/accounts/user_identity.ex
+++ lib/ash_learning/accounts/user_identity.ex
@@ -4,6 +4,8 @@ defmodule AshLearning.Accounts.UserIdentity do
     extensions: [AshAuthentication.UserIdentity],
     domain: AshLearning.Accounts
 
+  alias AshLearning.Accounts.UserIdentity.Actions.Disconnect
+
   user_identity do
     user_resource AshLearning.Accounts.User
   end
@@ -18,46 +20,13 @@ defmodule AshLearning.Accounts.UserIdentity do
       argument :user_id, :uuid, allow_nil?: false
       argument :provider, :string, allow_nil?: false
 
-      run fn input, _context ->
-        user_id = input.arguments.user_id
-        provider = input.arguments.provider
-
-        # Find and destroy the identity
-        case Ash.read(__MODULE__, domain: AshLearning.Accounts) do
-          {:ok, identities} ->
-            identity =
-              Enum.find(identities, fn id ->
-                id.user_id == user_id and id.strategy == provider
-              end)
-
-            if identity do
-              case Ash.destroy(identity, domain: AshLearning.Accounts) do
-                {:ok, _destroyed_identity} ->
-                  {:ok, :disconnected}
-
-                :ok ->
-                  {:ok, :disconnected}
-
-                {:error, error} ->
-                  {:error, error}
-              end
-            else
-              {:ok, :not_found}
-            end
-
-          {:error, error} ->
-            {:error, error}
-        end
-      end
+      run Disconnect
     end
   end
 
   attributes do
-    # Additional fields for storing provider user info
     attribute :email, :string, public?: true
     attribute :avatar_url, :string, public?: true
-
-    # Display name from provider
     attribute :provider_name, :string, public?: true
   end
 end
